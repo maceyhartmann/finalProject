15de1878efe58212eb40ed245f4cb4b5feccf0da
maceyhartmann <51975126+maceyhartmann@users.noreply.github.com>
2019-06-21 14:09:20 -0700

    added script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// 
+function toggleProjects(){
+  var projectList = document.getElementById("projectList");
+  if(projectList.style.display == 'none'){
+      //
+    projectList.style.display = 'block'
+  }
+  else{
+    projectList.style.display = 'none'
+  }
+}
+
+function toggleTech(){
+  var techList = document.getElementById("techList");
+  if(techList.style.display == 'none'){
+    techList.style.display = 'block'
+  }
+  else{
+    techList.style.display = 'none'
+  }
+}
